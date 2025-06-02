32644dba5bfff145edcdf28a052d037116979e67
Fedor <starbreeze@yandex.ru>
2025-06-03 00:02:57 +0300

    update custom cellview

---
+++ EmojiDictionary/EmojiTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiDictionary
+//
+//  Created by Федор Завьялов on 30.05.2025.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var emojiView: UILabel!
+    @IBOutlet weak var emoojiNamw: UILabel!
+    @IBOutlet weak var emojiDescription: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func update(with emoji: EmojiModel) {
+        emojiView.text = emoji.symbol
+        emoojiNamw.text = emoji.name
+        emojiDescription.text = emoji.description
+    }
+
+}
--- EmojiDictionary/EmojiTableViewController.swift
+++ EmojiDictionary/EmojiTableViewController.swift
@@ -25,12 +25,14 @@ class EmojiTableViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "EmojiCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EmojiCell", for: indexPath) as! EmojiTableViewCell
         let emoji = emojiGroups[indexPath.section].emojis[indexPath.row]
-        var content = cell.defaultContentConfiguration()
-        content.text = "\(emoji.symbol) - \(emoji.name)"
-        content.secondaryText = emoji.description
-        cell.contentConfiguration = content
+//        var content = cell.defaultContentConfiguration()
+//        content.text = "\(emoji.symbol) - \(emoji.name)"
+//        content.secondaryText = emoji.description
+//        cell.contentConfiguration = content
+        cell.update(with: emoji)
+        cell.showsReorderControl = true
         return cell
     }
     
